d15e57edf5617430ac307970b1ad65bb12bd7295
mp-web3 <mp.web3.t@gmail.com>
2023-03-02 23:20:14 +0100

    final code lottery

--- Contracts/Lottery.sol
+++ Contracts/Lottery.sol
@@ -18,10 +18,21 @@ contract Lottery {
         return uint(keccak256(block.difficulty, now, players)); //sha3 is a global variable, we pass in the sources of pseudom randomnes: block difficulty, current time and array of players 
     }
 
-    function pickWinner() public {
+    function pickWinner() public restricted {
+        // require(msg.sender == manager); //enforce that nobady can call pickWinner but the manager, the address that deployed the contract
+
         uint index = random() % players.length;
         players[index].transfer(this.balance); //players.index is the address of the winner on which we can call the method transfer; this.balance is the total balance in the contract
         players = new address[](0); //empty the list by creating a new array of type address with 0 elements
+    }
+
+    modifier restricted() { // function modifier are used to reduce the amount of code. By adding the name of the modifier to a function will add the code behind the curtains
+        require(msg.sender == manager);
+        _;
+    }
+
+    function getPlayers() public view returns (address[]) {
+        return players;
     }
 
 }
